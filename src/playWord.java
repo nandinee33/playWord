249e36ce09b172d26de31142906b12d545feccb6
gupta <nandinee.gupta_cs18@gla.ac.in>
2020-02-16 18:07:14 +0530

    Taking input from Scanner

---
+++ src/playWord.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class playWord {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        String b = sc.nextLine();
+
+    }
+}
