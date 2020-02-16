8a79eb7e04525188a8ac7a589e432ad987eee30d
gupta <nandinee.gupta_cs18@gla.ac.in>
2020-02-16 18:19:46 +0530

    final commit

--- src/playWord.java
+++ src/playWord.java
@@ -9,6 +9,18 @@ public class playWord {
         int d = b.length();
         int[] arr1 = new int[26];
         int[] arr2 = new int[26];
-
+        for (int i = 0; i < c; i++) {
+            arr1[a.charAt(i) - 97]++;
+        }
+        for (int j = 0; j < d; j++) {
+            arr2[b.charAt(j) - 97]++;
+        }
+        int r = 0;
+        for (int k = 0; k < 26; k++) {
+            if (arr1[k] != arr2[k]) {
+                r = r + Math.abs(arr1[k] - arr2[k]);
+            }
+        }
+        System.out.println(r);
     }
 }
